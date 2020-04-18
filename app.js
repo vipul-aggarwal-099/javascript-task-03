19d6a55672b61eff78682618df57dde55952b492
Vipul Aggarwal <vipul.captureart@gmail.com>
2020-04-19 00:01:38 +0530

    Update app.js with html.
    Completed Javascript task 03.

--- app.js
+++ app.js
@@ -1,27 +1,35 @@
 
-function javascriptTask(number1) {
-    const arr1 = [];
-    for(let i = 1; i <= number1; i++) {
-        if(i % 2 === 0) {
-            arr[i - 1] = 'yu';
-        }
-        if (i % 3 === 0) {
-            arr[i - 1] = "gi";
-        }
-        if (i % 5 === 0) {
-            arr1[i - 1] ="oh";
-        }
-        if (i % 2 === 0 && i % 3 === 0) {
-            arr1[i - 1] = "yu-gi";
-        }
-        if (i % 2 == 0 && i % 5 == 0) {
-            arr1[i - 1] = "yu-oh";
-        }
-        if (i % 3 === 0 && i % 5 === 0) {
-            arr[i - 1] = "gi-oh";
-        }
+function javascriptTask02(checkNum) {
+    let arr1 = [];
+    for(let i = 1; i <= checkNum; i++) {
         if (i % 2 === 0 && i % 3 === 0 && i % 5 === 0) {
-            arr1[i - 1] = "yu-gi-oh";
+            arr1.push("yu-gi-oh");
+        }
+        else if (i % 2 === 0 && i % 3 === 0) {
+            arr1.push("yu-gi");
+        }
+        else if (i % 2 == 0 && i % 5 == 0) {
+            arr1.push("yu-oh");
+        }
+        else if (i % 3 === 0 && i % 5 === 0) {
+            arr1.push("gi-oh");
+        }
+        else if(i % 2 === 0) {
+            arr1.push("yu");
+        }
+        else if (i % 3 === 0) {
+            arr1.push("gi");
+        }
+        else if (i % 5 === 0) {
+            arr1.push("oh");
+        }
+        else {
+            arr1.push(i);
         }
     }
+    console.log(arr1);
 }
+
+
+
+
